618342d82cc9266a774c3f54941158b4ef3db29f
Brian <juniortoad1738@gmail.com>
2017-11-06 22:45:10 -0500

    chapter 3 finished

---
+++ app/src/main/java/com/junior/brianphelps/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.junior.brianphelps.geoquiz;
+
+/**
+ * Created by brianphelps on 11/6/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
